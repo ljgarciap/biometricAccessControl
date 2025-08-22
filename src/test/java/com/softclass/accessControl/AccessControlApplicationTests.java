25d9fd65817333358baada17f0ab25a45b7826d0
Luis Garcia <lujogarpin78@gmail.com>
2025-08-22 00:08:56 -0500

    first commit

---
+++ src/test/java/com/softclass/accessControl/AccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softclass.accessControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
